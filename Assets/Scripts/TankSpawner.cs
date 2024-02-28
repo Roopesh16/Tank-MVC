84e4fe8b3a593ed13405ff20ffafbb93b89fc904
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-28 16:20:23 +0530

    Set tank type and its data

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -1,11 +1,21 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class Tank
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public TankTypes tankType;
+    public Material color;
+}
 
 public class TankSpawner : MonoBehaviour
 {
     [SerializeField] private TankView tankPrefab;
-    
+    [SerializeField] private List<Tank> tanks = new List<Tank>();
+
     void Start()
     {
         CreateTank();
@@ -13,7 +23,7 @@ public class TankSpawner : MonoBehaviour
 
     private void CreateTank()
     {
-        TankModel tankModel = new TankModel(30,20);
+        TankModel tankModel = new TankModel(tanks[2].movementSpeed, tanks[2].rotationSpeed, tanks[2].tankType, tanks[2].color);
         TankController tankController = new TankController(tankModel, tankPrefab);
     }
 }

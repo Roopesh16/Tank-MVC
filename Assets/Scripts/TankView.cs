1263ca98b73a78eefc37979201a17311f9f459ee
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-28 14:06:01 +0530

    Take user input

--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -1,24 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TankView : MonoBehaviour
 {
+    [SerializeField] private Rigidbody tankRb;
     private TankController tankController;
-    // Start is called before the first frame update
+    private float movement;
+    private float rotation;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Movement();
+
+        if(movement!=0)
+            tankController.Move(movement, 30f);
         
+        if(rotation!=0)
+            tankController.Rotate(rotation, 20f);
     }
 
     public void SetTankController(TankController tankController)
     {
         this.tankController = tankController;
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return tankRb;
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
 }

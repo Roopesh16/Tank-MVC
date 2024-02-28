77cae571d745d357ef26b3473503d9bde2f5e62f
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-28 12:25:12 +0530

    Add create tank function

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject tankPrefab;
-    // Start is called before the first frame update
+    [SerializeField] private TankView tankPrefab;
+    
     void Start()
     {
-        Instantiate(tankPrefab,transform.position,Quaternion.identity);
+        CreateTank();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateTank()
     {
-        
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankPrefab);
     }
 }

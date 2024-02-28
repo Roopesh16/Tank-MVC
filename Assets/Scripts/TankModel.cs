5ebc926170b7fcbcdc1f46562dcf5e05c49f274e
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-28 15:23:53 +0530

    Set move and roate var

--- Assets/Scripts/TankModel.cs
+++ Assets/Scripts/TankModel.cs
@@ -1,10 +1,13 @@
 public class TankModel
 {
     private TankController tankController;
+    public float movement;
+    public float rotation; 
 
-    public TankModel()
+    public TankModel(float movement,float rotation)
     {
-
+        this.movement = movement;
+        this.rotation = rotation;
     }
 
     public void SetTankController(TankController tankController)

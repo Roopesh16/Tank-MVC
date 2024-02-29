51288ffd2b3b0058a36ef0a786626011416a1d27
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-29 14:11:34 +0530

    Set to bullet spawn

--- Assets/Scripts/Bullet/BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawner.cs
@@ -16,9 +16,9 @@ public class BulletSpawner : MonoBehaviour
     public BulletView bulletPrefab;
     public List<Bullet> bullets = new List<Bullet>();
     
-    public void CreateBullet(TankTypes tankTypes)
+    public void CreateBullet(BulletType bulletTypes)
     {
-        int bulId = (int)tankTypes;
+        int bulId = (int)bulletTypes;
         BulletModel bulletModel = new BulletModel(bullets[bulId].bulletSpeed, bullets[bulId].bulletType, bullets[bulId].color);
         BulletController bulletController = new BulletController(bulletModel, bulletPrefab);
     }

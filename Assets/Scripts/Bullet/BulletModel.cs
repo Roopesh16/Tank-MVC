9c15a5dc946eae4e0342aa09f72e9d0f9cf85d75
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-29 14:28:26 +0530

    Spawn bullet with tank

--- Assets/Scripts/Bullet/BulletModel.cs
+++ Assets/Scripts/Bullet/BulletModel.cs
@@ -7,11 +7,17 @@ public class BulletModel
     public float bulletSpeed;
     public BulletType bulletType;
     public Material color;
+    private BulletController bulletController;
 
     public BulletModel(float bulletSpeed,BulletType bulletType,Material color)
     {
         this.bulletSpeed = bulletSpeed;
         this.bulletType = bulletType;
         this.color = color;
+    }
+
+    public void SetBulletController(BulletController bulletController)
+    {
+        this.bulletController = bulletController;
     }
 }
--- Assets/Scripts/Tank/TankSelection.cs
+++ Assets/Scripts/Tank/TankSelection.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 public class TankSelection : MonoBehaviour
 {
     [SerializeField] private TankSpawner tankSpawner;
+    [SerializeField] private BulletSpawner bulletSpawner;
 
     public void GreenTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.GREEN);
+        bulletSpawner.CreateBullet(BulletType.GREEN);
         gameObject.SetActive(false);
     }
 
     public void RedTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.RED);
+        bulletSpawner.CreateBullet(BulletType.RED);
         gameObject.SetActive(false);
     }
 
     public void BlueTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.BLUE);
+        bulletSpawner.CreateBullet(BulletType.BLUE);
         gameObject.SetActive(false);
     }
 }

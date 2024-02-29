c0ee7da622c9b6a4e2be7961c8364aa1e327bdf5
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-29 14:01:34 +0530

    Set B controller const

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BulletController : MonoBehaviour
+public class BulletController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private BulletModel bulletModel;
+    private BulletView bulletView;
 
-    // Update is called once per frame
-    void Update()
+    public BulletController(BulletModel bulletModel,BulletView bulletView)
     {
+        this.bulletModel = bulletModel;
+        this.bulletView = bulletView;
         
     }
 }

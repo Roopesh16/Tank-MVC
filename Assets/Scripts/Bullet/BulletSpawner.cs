c8cc99f459901212af653ac9c6b0747da12b7be9
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-29 14:01:50 +0530

    Setup bullet spawner

--- Assets/Scripts/Bullet/BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawner.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class Bullet
+{
+    public float bulletSpeed;
+    public BulletType bulletType;
+    public Material color;
+}
+
 public class BulletSpawner : MonoBehaviour
 {
+    public BulletView bulletPrefab;
+    public List<Bullet> bullets = new List<Bullet>();
     
-    public void CreateBullet()
+    public void CreateBullet(TankTypes tankTypes)
     {
-
+        int bulId = (int)tankTypes;
+        BulletModel bulletModel = new BulletModel(bullets[bulId].bulletSpeed, bullets[bulId].bulletType, bullets[bulId].color);
+        BulletController bulletController = new BulletController(bulletModel, bulletPrefab);
     }
 }

2ece0dab00d8eadc9e3c662ecd5db6283b8662c5
Roopesh Verma <roopeshv.30@gmail.com>
2024-02-29 13:44:58 +0530

    Create bullet folder script

---
+++ Assets/Scripts/Bullet/BulletView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletView : MonoBehaviour
+{
+    bool hasTankSpawned = false;
+
+    void Update()
+    {
+        if(hasTankSpawned)
+        {
+
+        }        
+    }
+
+    public void SetBullet()
+    {
+        
+    }
+}
--- Assets/Scripts/TankTypes.cs
+++
@@ -1,6 +0,0 @@
-public enum TankTypes
-{
-    GREEN,
-    BLUE,
-    RED
-}

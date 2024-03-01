22e66c75cf349c1e5d163de8033e54fea4b6a0e1
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-01 15:27:31 +0530

    Add list of spawn position

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private int enemyCount = 3;
     [SerializeField] private EnemyView enemyPrefab;
+    [SerializeField] private List<Transform> spawnPositions = new List<Transform>();
     [SerializeField] private EnemyScriptableObject[] enemyScriptableObject;
 
     public void PoolEnemyTanks()
@@ -14,14 +15,14 @@ public class EnemySpawner : MonoBehaviour
         {
             int id = Random.Range(0, 3);
             EnemyModel enemyModel = new EnemyModel(enemyScriptableObject[id].enemyType,
-                                                 enemyScriptableObject[id].movementSpeed,
-                                                enemyScriptableObject[id].rotationSpeed, 
-                                                enemyScriptableObject[id].health, 
-                                                enemyScriptableObject[id].firingRate, 
-                                                enemyScriptableObject[id].stoppingDistance, 
-                                                enemyScriptableObject[id].tankColor);
+                                                   enemyScriptableObject[id].movementSpeed,
+                                                   enemyScriptableObject[id].rotationSpeed, 
+                                                   enemyScriptableObject[id].health, 
+                                                   enemyScriptableObject[id].firingRate, 
+                                                   enemyScriptableObject[id].stoppingDistance, 
+                                                   enemyScriptableObject[id].tankColor);
 
-            EnemyController enemyController = new EnemyController(enemyModel, enemyPrefab);
+            EnemyController enemyController = new EnemyController(enemyModel, enemyPrefab,spawnPositions[i]);
 
         }
     }

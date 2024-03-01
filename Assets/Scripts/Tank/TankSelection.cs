656794cae07d8c47df0cf384211ebe8ede3b3a83
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-01 12:22:04 +0530

    Use tank type to set bullet

--- Assets/Scripts/Tank/TankSelection.cs
+++ Assets/Scripts/Tank/TankSelection.cs
@@ -8,21 +8,21 @@ public class TankSelection : MonoBehaviour
     public void GreenTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.GREEN);
-        bulletSpawner.CreateBullet(BulletType.High_Explosive,tankSpawner.GetBulletSpawn());
+        bulletSpawner.CreateBullet(TankTypes.GREEN,tankSpawner.GetBulletSpawn());
         gameObject.SetActive(false);
     }
 
     public void RedTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.RED);
-        bulletSpawner.CreateBullet(BulletType.Armour_Piercing,tankSpawner.GetBulletSpawn());
+        bulletSpawner.CreateBullet(TankTypes.RED,tankSpawner.GetBulletSpawn());
         gameObject.SetActive(false);
     }
 
     public void BlueTankSelection()
     {
         tankSpawner.CreateTank(TankTypes.BLUE);
-        bulletSpawner.CreateBullet(BulletType.Guided_Missile,tankSpawner.GetBulletSpawn());
+        bulletSpawner.CreateBullet(TankTypes.BLUE,tankSpawner.GetBulletSpawn());
         gameObject.SetActive(false);
     }
 }

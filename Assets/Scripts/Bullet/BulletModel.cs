19b922d4fecdcfe992aa8dfc576bb2f566b0629b
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-01 12:20:26 +0530

    Add damage rad and firing rate

--- Assets/Scripts/Bullet/BulletModel.cs
+++ Assets/Scripts/Bullet/BulletModel.cs
@@ -7,13 +7,18 @@ public class BulletModel
     public float bulletSpeed;
     public BulletType bulletType;
     public Material color;
+    public float damageRadius;
+    public float firingRate;
+    
     private BulletController bulletController;
 
-    public BulletModel(float bulletSpeed,BulletType bulletType,Material color)
+    public BulletModel(float bulletSpeed,BulletType bulletType,Material color,float damageRadius,float firingRate)
     {
         this.bulletSpeed = bulletSpeed;
         this.bulletType = bulletType;
         this.color = color;
+        this.damageRadius = damageRadius;
+        this.firingRate = firingRate;
     }
 
     public void SetBulletController(BulletController bulletController)

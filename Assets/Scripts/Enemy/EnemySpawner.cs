bcdaef31843e2cc0a9a5727d364f9d5e9bf968cb
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-01 15:15:37 +0530

    Pool enemy tanks and set data

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private int enemyCount = 3;
+    [SerializeField] private EnemyView enemyPrefab;
+    [SerializeField] private EnemyScriptableObject[] enemyScriptableObject;
+
+    public void PoolEnemyTanks()
+    {
+        for (int i = 0; i < enemyCount;i++)
+        {
+            int id = Random.Range(0, 3);
+            EnemyModel enemyModel = new EnemyModel(enemyScriptableObject[id].enemyType,
+                                                 enemyScriptableObject[id].movementSpeed,
+                                                enemyScriptableObject[id].rotationSpeed, 
+                                                enemyScriptableObject[id].health, 
+                                                enemyScriptableObject[id].firingRate, 
+                                                enemyScriptableObject[id].stoppingDistance, 
+                                                enemyScriptableObject[id].tankColor);
+
+            EnemyController enemyController = new EnemyController(enemyModel, enemyPrefab);
+
+        }
+    }
+}

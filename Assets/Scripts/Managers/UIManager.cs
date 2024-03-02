f708aeaf33c6c8ab0e21c53f64bbbe074ca6f6e7
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-02 19:18:53 +0530

    make singleton

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static UIManager instance = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 }

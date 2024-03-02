4963af01c1ce2c414ffc3eae3637de6661fe7f5e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-02 17:02:46 +0530

    Keep a list of enemy controllers and spawn one by one

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private int enemyCount = 3;
     [SerializeField] private List<Transform> spawnPositions = new List<Transform>();
     [SerializeField] private EnemyScriptableObject[] enemyList;
 
-    public void PoolEnemyTanks(int damage)
+    private List<EnemyController> enemyControllers = new();
+
+    private int currentEnemy = 0;
+
+    public void PoolEnemyTanks(int enemyCount, int damage)
     {
         for (int i = 0; i < enemyCount; i++)
         {
             int id = Random.Range(0, 3);
             EnemyModel enemyModel = new EnemyModel(enemyList[id].enemyType,
-                                                   enemyList[id].movementSpeed,
-                                                   enemyList[id].rotationSpeed,
-                                                   enemyList[id].health,
-                                                   enemyList[id].firingRate,
-                                                   enemyList[id].stoppingDistance);
+                                                       enemyList[id].movementSpeed,
+                                                       enemyList[id].rotationSpeed,
+                                                       enemyList[id].health,
+                                                       enemyList[id].firingRate,
+                                                       enemyList[id].stoppingDistance);
 
-            EnemyController enemyController = new EnemyController(enemyModel, enemyList[id].enemyPrefab, spawnPositions[i],damage);
-
+            EnemyController enemyController = new EnemyController(enemyModel, enemyList[id].enemyPrefab, spawnPositions[i], damage);
+            enemyControllers.Add(enemyController);
         }
+
+        SpawnNextTank();
     }
+
+    private void SpawnNextTank()
+    {
+        enemyControllers[currentEnemy].EnableTank();
+    }
+
 }

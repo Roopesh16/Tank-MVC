0baf591f4307a95c1b4b2a7654f4a2dce57944be
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-03 14:47:02 +0530

    destroy bullet after max time

--- Assets/Scripts/Enemy/EnemyBulletView.cs
+++ Assets/Scripts/Enemy/EnemyBulletView.cs
@@ -1,15 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyBulletView : MonoBehaviour
 {
     private float bulletSpeed;
     private bool canMove = false;
-    void Update()
+    private float timer = 0f;
+    private float maxTime = 3f;
+
+    private void Update()
     {
         if (canMove)
         {
             transform.Translate(transform.forward * bulletSpeed * Time.deltaTime, Space.World);
+
+            timer += Time.deltaTime;
+            if (timer >= maxTime)
+                Destroy(gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            Debug.Log("hit");
+        Destroy(gameObject);
     }
 
     public void SetEnemyBulletView(Vector3 forward, float bulletSpeed)

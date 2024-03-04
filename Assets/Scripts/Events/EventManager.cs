b7daf69719bcad92d4f0b8d9e9d64a92a27c4d90
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-04 19:29:12 +0530

    use game manager to call

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -4,17 +4,8 @@ public class EventManager
 {
     private static EventManager instance = null;
 
-    public static EventManager Instance
-    {
-        get
-        {
-            instance ??= new EventManager();
-
-            return instance;
-        }
-    }
-
     public EventController OnGameOver { get; private set; }
+    public EventController OnNewWave { get; private set; }
 
     public EventManager()
     {

f0191aa4ddcd1659efba87fc5f0ae55e6c1a23b6
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-04 19:30:55 +0530

    remove event manager instance

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -2,8 +2,6 @@ using Unity.VisualScripting;
 
 public class EventManager
 {
-    private static EventManager instance = null;
-
     public EventController OnGameOver { get; private set; }
     public EventController OnNewWave { get; private set; }
 

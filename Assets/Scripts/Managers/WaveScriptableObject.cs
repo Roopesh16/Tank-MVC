8045da29f68db904c10f36f4594f3c569ea1307b
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-15 21:50:34 +0530

    Add enemies to spawn to each Wave

--- Assets/Scripts/Managers/WaveScriptableObject.cs
+++ Assets/Scripts/Managers/WaveScriptableObject.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "WaveScriptableObject", menuName = "Waves/WaveScriptableObject")]
 public class WaveScriptableObject : ScriptableObject
 {
     public int waveNumber;
     public int enemyCount;
+    public List<EnemyType> enemiesToSpawn = new ();
 }

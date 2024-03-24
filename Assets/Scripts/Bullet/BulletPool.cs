4cfe13ae79474f2763fe2eea9858bfeb31f88de8
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-24 17:43:58 +0530

    Add bullet pool

---
+++ Assets/Scripts/Bullet/BulletPool.cs
@@ -0,0 +1,29 @@
+using Utility;
+using UnityEngine;
+
+namespace Bullet
+{
+    public class BulletPool : GenericObjectPool<BulletController>
+    {
+        private BulletModel bulletModel;
+        private BulletView bulletView;
+        private Transform spawnPoint;
+        private Transform parentPosition;
+
+        public BulletController GetBullet(BulletModel bulletModel, BulletView bulletView,
+            Transform spawnPoint, Transform parentPosition)
+        {
+            this.bulletModel = bulletModel;
+            this.bulletView = bulletView;
+            this.spawnPoint = spawnPoint;
+            this.parentPosition = parentPosition;
+
+            return GetItem();
+        }
+
+        protected override BulletController CreateItem()
+        {
+            return new BulletController(bulletModel,bulletView,spawnPoint,parentPosition);
+        }
+    }
+}

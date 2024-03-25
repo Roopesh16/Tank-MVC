dc1bba7440d5273493bdc22c98154bba635f7752
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-25 22:10:51 +0530

    Add dictionary to bind type with controller

--- Assets/Scripts/Bullet/BulletSpawner.cs
+++ Assets/Scripts/Bullet/BulletSpawner.cs
@@ -1,31 +1,51 @@
+using System;
 using UnityEngine;
 using Bullet;
+using System.Collections.Generic;
 
 public class BulletSpawner : MonoBehaviour
 {
     [SerializeField] private BulletView bulletPrefab;
     [SerializeField] private BulletScriptableObject[] bulletList;
+    
     private bool hasTankSpawned = false;
     private Transform bulletSpawnPoint;
     private BulletPool bulletPool;
     private BulletModel bulletModel;
+    private TankTypes tankType;
+
+    private Dictionary<TankTypes, BulletController> bulletDict = new();
     
     public void CreateBullet(TankTypes tankType, Transform bulletSpawnPoint)
     {
         int bId = (int)tankType;
+        this.tankType = tankType;
         bulletPool = new BulletPool();    
         bulletModel = new BulletModel(bulletList[bId]);
         this.bulletSpawnPoint = bulletSpawnPoint;
         hasTankSpawned = true;
+        InitBulletDictionary();
     }
 
     private void Update()
     {
         if (hasTankSpawned && Input.GetKeyDown(KeyCode.Space))
         {
-            BulletController bulletController = bulletPool.GetBullet(bulletModel, bulletPrefab, bulletSpawnPoint, transform);
+            BulletController bulletController = bulletDict[tankType];
             bulletController.ShootBullet();
         }
+    }
+
+    private void InitBulletDictionary()
+    {
+        bulletDict.Add(TankTypes.RED, bulletPool.GetBullet<ArmourBulletController>(bulletModel, 
+                                                    bulletPrefab, bulletSpawnPoint, transform));
+        
+        bulletDict.Add(TankTypes.GREEN, bulletPool.GetBullet<ExplosiveBulletController>(bulletModel, 
+                                                     bulletPrefab, bulletSpawnPoint, transform));
+        
+        bulletDict.Add(TankTypes.BLUE, bulletPool.GetBullet<GuidedBulletController>(bulletModel, 
+                                                    bulletPrefab, bulletSpawnPoint, transform));
     }
     
     public int GetDamage() => bulletModel.damage;

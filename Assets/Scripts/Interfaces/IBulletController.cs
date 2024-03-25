5150389eeaaf13d5db9c31449eb29bbbd31b6e10
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-25 20:31:58 +0530

    Remove interface

--- Assets/Scripts/Interfaces/IBulletController.cs
+++
@@ -1,9 +0,0 @@
-namespace Interfaces
-{
-    public interface IBulletController
-    {
-        public void ShootBullet();
-        public void OnBulletHit();
-        public void DisableBullet();
-    }
-}

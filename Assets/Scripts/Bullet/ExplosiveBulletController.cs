aac360a7640c0b0ed2ac10e95123726818cd9b9e
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-25 20:41:22 +0530

    Add explosive bullet controller

---
+++ Assets/Scripts/Bullet/ExplosiveBulletController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Bullet
+{
+    public class ExplosiveBulletController : BulletController
+    {
+        private BulletModel bulletModel;
+        private BulletView bulletSpawned;
+        private Transform parentPosition;
+        private Transform spawnPoint;
+    
+        public ExplosiveBulletController(BulletModel bulletModel, BulletView bulletPrefab, Transform spawnPoint, Transform parentPosition)
+                :base(bulletModel,bulletPrefab,spawnPoint,parentPosition)
+        {
+            this.bulletModel = bulletModel;
+            this.bulletModel.SetBulletController(this);
+            this.spawnPoint = spawnPoint;
+        
+            bulletSpawned = GameObject.Instantiate<BulletView>(bulletPrefab,spawnPoint);
+            bulletSpawned.InitBulletView(bulletModel.color, bulletModel.blastRadius,bulletModel.bulletSpeed);
+            bulletSpawned.SetBulletController(this);
+            bulletSpawned.transform.SetParent(parentPosition);
+        }
+    
+        public override void ShootBullet()
+        {
+            bulletSpawned.gameObject.SetActive(true);
+            bulletSpawned.gameObject.transform.position = spawnPoint.position;
+            // bulletSpawned.gameObject.transform.forward = spawnPoint.forward;
+            bulletSpawned.ShootBullet();
+        }
+
+        public override void OnBulletHit()
+        {
+            bulletSpawned.BlastImpact();
+            bulletSpawned.GetBlastParticle().Play();
+            bulletSpawned.DisableMesh();
+            bulletSpawned.StopBullet(spawnPoint);
+            // Invoke("DisableBullet",bulletSpawned.GetBlastParticle().main.duration);
+            DisableBullet();
+        }
+
+        protected override void DisableBullet()
+        {
+            bulletSpawned.gameObject.SetActive(false);
+            GameManager.Instance.GetBulletSpawner().ReturnBulletToPool(this);
+        }
+    }
+}

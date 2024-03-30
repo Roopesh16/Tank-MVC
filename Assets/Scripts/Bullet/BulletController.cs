a314b07cef29d328a0e14d8a13fd818ceb3edda8
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-30 22:08:51 +0530

    Add common part in parent class

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -4,9 +4,47 @@ namespace Bullet
 {
     public class BulletController
     {
-        public BulletController(BulletModel bulletModel, BulletView bulletPrefab, Transform spawnPoint, Transform parentPosition){}
-        public virtual void ShootBullet(){}
-        public virtual void OnBulletHit(){}
-        protected virtual void DisableBullet(){}
+        protected BulletModel bulletModel;
+        protected BulletView bulletSpawned;
+        protected Transform parentPosition;
+        protected Transform spawnPoint;
+        
+        public BulletController(){}
+
+        public BulletController(BulletModel bulletModel, BulletView bulletPrefab, Transform spawnPoint,
+            Transform parentPosition)
+        {
+            this.bulletModel = bulletModel;
+            this.spawnPoint = spawnPoint;
+            this.parentPosition = parentPosition;
+            
+            bulletSpawned = GameObject.Instantiate<BulletView>(bulletPrefab,spawnPoint);
+            bulletSpawned.InitBulletView(bulletModel.color, bulletModel.blastRadius,bulletModel.bulletSpeed);
+            bulletSpawned.SetBulletController(this);
+            bulletSpawned.transform.SetParent(parentPosition);
+        }
+
+        public virtual void ShootBullet()
+        {
+            bulletSpawned.gameObject.SetActive(true);
+            bulletSpawned.gameObject.transform.position = spawnPoint.position;
+            // bulletSpawned.gameObject.transform.forward = spawnPoint.forward;
+            bulletSpawned.ShootBullet();
+        }
+
+        public virtual void OnBulletHit()
+        {
+            bulletSpawned.BlastImpact();
+            bulletSpawned.GetBlastParticle().Play();
+            bulletSpawned.DisableMesh();
+            bulletSpawned.StopBullet(spawnPoint);
+            // Invoke("DisableBullet",bulletSpawned.GetBlastParticle().main.duration);
+            DisableBullet();
+        }
+
+        protected virtual void DisableBullet()
+        {
+            bulletSpawned.gameObject.SetActive(false);
+        }
     }
 }
